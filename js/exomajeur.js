c777a54a96a10b8e29b27a551d2deedc8722657e
Jordan Ricca <jordan.ricca@lepoles.org>
2024-07-25 15:15:27 +0200

    javaexo

---
+++ js/exomajeur.js
@@ -0,0 +1,8 @@
+let user = prompt("Qu'elle âge avez vous?").toLowerCase();
+
+if (user >= 18) {
+    alert("ça passe");
+} else if (user < 18) {
+    alert(user = "ça dégage");
+    window.location.href="https://www.toysrus.fr/";
+}
